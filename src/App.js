320e44f2fac34d76feeb91e06d83ecf48e1e5a66
Yinon Bar <yinonbar1988@gmail.com>
2022-11-18 17:37:20 +0200

    stats working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 import FeedbackForm from "./Components/FeedbackForm";
 import FeedbackList from "./Components/FeedbackList";
@@ -15,11 +16,16 @@ function App() {
     }
   };
 
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
   return (
     <div className="App">
       <Header text="" />
       <div className="container">
-        <FeedbackForm />
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
--- src/Components/FeedbackForm.js
+++ src/Components/FeedbackForm.js
@@ -1,19 +1,47 @@
 import { useState } from "react";
+import RatingSelect from "./RatingSelect";
 import Button from "./Shared/Button";
 import Card from "./Shared/Card";
 
-function FeedbackForm() {
+function FeedbackForm({ handleAdd }) {
   const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
+  const [btnDisabled, setbtnDisabled] = useState(true);
+  const [msg, setMsg] = useState("");
 
   const inputHandler = (event) => {
-    // setText()
+    if (text.length === "") {
+      setbtnDisabled(true);
+      setMsg(null);
+    } else if (text.length >= 0 && text.length < 10) {
+      setbtnDisabled(true);
+      setMsg("Your feedback must have 10 or more characters");
+    } else {
+      setMsg(null);
+      setbtnDisabled(false);
+    }
     setText(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      };
+
+      handleAdd(newFeedback);
+      setText("");
+      setRating(0);
+    }
   };
 
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How Would You Rate Your Service With Us?</h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
           <input
             onChange={inputHandler}
@@ -21,8 +49,12 @@ function FeedbackForm() {
             type="text"
             placeholder="Write a Review"
           />
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+
+        {msg && <div className="message">{msg}</div>}
       </form>
     </Card>
   );
