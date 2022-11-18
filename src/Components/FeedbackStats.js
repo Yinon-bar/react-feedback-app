6230a0649f8a48bd28b0aaad43e1ccb4df96631d
Yinon Bar <yinonbar1988@gmail.com>
2022-11-18 15:53:05 +0200

    stats working

--- src/Components/FeedbackStats.js
+++ src/Components/FeedbackStats.js
@@ -1,14 +1,15 @@
 function FeedbackStats({ feedback }) {
-  const average = feedback.reduce((acc, cur) => {
+  let sum = feedback.reduce((acc, cur) => {
     return acc + cur.rating;
   }, 0);
 
-  console.log(average);
+  let average = sum / feedback.length;
+  average = average.toFixed(0);
 
   return (
     <div className="feedback-stats">
       <h4>{feedback.length} Reviews</h4>
-      <h4>Average Rating: 9.4</h4>
+      <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
     </div>
   );
 }
